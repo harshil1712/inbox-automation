6780e0d7f5d04ab2f843099384a716cc8a57fd5c
Harshil <ghagrawal17@gmail.com>
2025-05-04 04:09:21 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import * as PostalMime from 'postal-mime';
+import { extractText, getDocumentProxy } from "unpdf";
+
+interface EmailAttachment {
+	filename: string;
+	content: ArrayBuffer;
+	contentType: string;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+	async email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext) {
+		let contentForAi = '';
+		const parser = new PostalMime.default({
+			attachmentEncoding: 'arraybuffer'
+		});
+		const rawEmail = new Response(message.raw);
+		const email = await parser.parse(await rawEmail.arrayBuffer());
+
+		console.log(email.attachments)
+
+		// ToDo: If attachments contains two files that have invoice and receipt in the filename only process the receipt file
+		if (email.attachments.length !== 0) {
+			for (const attachment of email.attachments) {
+				if (attachment.mimeType === 'application/pdf') {
+					const pdf = await extractText(await getDocumentProxy(attachment.content), { mergePages: true });
+					console.log(pdf);
+					// Remove all perrsonal information like name, email
+					// ToDo: Remove card number
+					// ToDo: Make this more dynamic
+					contentForAi = pdf.text.replace(env.YOUR_NAME, 'Payee Name').replace(env.YOUR_EMAIL, 'Payee Email').replace(env.YOUR_ADDRESS, 'Payee Address').replace(env.YOUR_POSTAL_CODE, 'Payee Postal Code');
+				}
+			}
+		}
+
+		console.log(contentForAi);
+	}
 } satisfies ExportedHandler<Env>;
